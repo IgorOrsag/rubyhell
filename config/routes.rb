8c2147bc72d9c757364335bcce41bed3d7641ebf
igor <igor.orsag@gmail.com>
2016-02-03 22:49:48 +0100

    as filter

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :tags
   root 'posts#index'
 
-  get 'posts/filter/:tag' => 'posts#filter'
+  get 'posts/filter/:tag' => 'posts#filter', as: :filter
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
